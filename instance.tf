bd13673cf098fade13a79a86fdc661c8e1d08d4e
ravalirao1 <84679049+ravalirao1@users.noreply.github.com>
2021-05-27 18:22:29 -0400

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -6,7 +6,7 @@ provider "aws" {
 resource "aws_instance" "myfirstInstance" {
     ami  = "ami-00399ec92321828f5"
     count = 1
-    key_name = "test-key"
+    key_name = "first key"
     instance_type = "t2.micro"
     
 
