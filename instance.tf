966a71eb5b8fbbe1d46ae28fe846f73a1bba45ed
ravalirao1 <84679049+ravalirao1@users.noreply.github.com>
2021-05-27 18:17:31 -0400

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,3 +1,8 @@
+provider "aws" {
+  region     = "us-east-2"
+  access_key = "AKIA5OGNS63WL3LBL7HR"
+  secret_key = "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"
+}
 resource "aws_instance" "myfirstInstance" {
     ami  = "ami-00399ec92321828f5"
     count = 1
